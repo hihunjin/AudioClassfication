b1f5e5ee2e42fd3bbc745eed2754d3c546ec2086
hihunjin <hihunjin@hanmail.net>
2023-11-03 15:34:06 +0000

    To 0.0.2

--- make_one_json.py
+++ make_one_json.py
@@ -21,8 +21,8 @@ def load_json(
 
 def main():
     all_data = {}
-    jsons = glob.glob("/mnt/ebs/data/kpf*/page*/_0.0.2_simple.json")
-    jsons.append("/mnt/ebs/data/kpf/all_231022_golden.json")
+    jsons = glob.glob("/mnt/ebs/data/kpf0.0.2/page*/_0.0.2_simple.json")
+    # jsons.append("/mnt/ebs/data/kpf/all_231022_golden.json")
     jsons.sort(key=os.path.getmtime)
     for json_path in jsons:
         print("json_path", json_path)
@@ -30,7 +30,7 @@ def main():
         all_data.update(data)
         print(len(all_data))
     
-    save_json("/mnt/ebs/data/all_231027.json", all_data)
+    save_json("/mnt/ebs/data/all_231104.json", all_data)
 
 
 if __name__ == "__main__":
