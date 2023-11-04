0e67e32d2ceb3ad49c3136a1c319f175540d6f49
hihunjin <hihunjin@hanmail.net>
2023-11-04 08:01:45 +0000

    Fix: black

--- scripts/inference.py
+++ scripts/inference.py
@@ -32,8 +32,8 @@ def run():
     try:
         args = vars(args)
     except:
-        if 'net' in args.keys():
-            del args['net']
+        if "net" in args.keys():
+            del args["net"]
         args_orig = args
         args = {}
         for k, v in args_orig.items():
@@ -42,47 +42,50 @@ def run():
                     args[kk] = vv
             else:
                 args[k] = v
-    args['f_res'] = f_res
-    args['add_noise'] = add_noise
+    args["f_res"] = f_res
+    args["add_noise"] = add_noise
     args["infer_json_path"] = _args.infer_json_path
-    with open(args['f_res'] / "args.yml", "w") as f:
+    with open(args["f_res"] / "args.yml", "w") as f:
         yaml.dump(args, f)
     print(args)
     #######################
     # Load PyTorch Models #
     #######################
     from modules.soundnet import SoundNetRaw as SoundNet
-    ds_fac = np.prod(np.array(args['ds_factors'])) * 4
-    net = SoundNet(nf=args['nf'],
-                   dim_feedforward=args['dim_feedforward'],
-                   clip_length=args['seq_len'] // ds_fac,
-                   embed_dim=args['emb_dim'],
-                   n_layers=args['n_layers'],
-                   nhead=args['n_head'],
-                   n_classes=args['n_classes'],
-                   factors=args['ds_factors'],
-                   )
 
-    print('***********************************************')
-    print("#params: {}M".format(count_parameters(net)/1e6))
+    ds_fac = np.prod(np.array(args["ds_factors"])) * 4
+    net = SoundNet(
+        nf=args["nf"],
+        dim_feedforward=args["dim_feedforward"],
+        clip_length=args["seq_len"] // ds_fac,
+        embed_dim=args["emb_dim"],
+        n_layers=args["n_layers"],
+        nhead=args["n_head"],
+        n_classes=args["n_classes"],
+        factors=args["ds_factors"],
+    )
+
+    print("***********************************************")
+    print("#params: {}M".format(count_parameters(net) / 1e6))
     if torch.cuda.is_available() and device == torch.device("cuda"):
-        t_b1 = measure_inference_time(net, torch.randn(1, 1, args['seq_len']))[0]
-        print('inference time batch=1: {:.2f}[ms]'.format(t_b1))
+        t_b1 = measure_inference_time(net, torch.randn(1, 1, args["seq_len"]))[0]
+        print("inference time batch=1: {:.2f}[ms]".format(t_b1))
         # t_b32 = measure_inference_time(net, torch.randn(32, 1, args['seq_len']))[0]
         # print('inference time batch=32: {:.2f}[ms]'.format(t_b32))
-        print('***********************************************')
+        print("***********************************************")
 
     if (f_res / Path("chkpnt.pt")).is_file():
         chkpnt = torch.load(f_res / "chkpnt.pt", map_location=torch.device(device))
-        model = chkpnt['model_dict']
+        model = chkpnt["model_dict"]
     else:
         raise ValueError
 
-    if 'use_dp' in args.keys() and args['use_dp']:
+    if "use_dp" in args.keys() and args["use_dp"]:
         from collections import OrderedDict
+
         state_dict = OrderedDict()
         for k, v in model.items():
-            name = k.replace('module.', '')
+            name = k.replace("module.", "")
             state_dict[name] = v
         net.load_state_dict(state_dict, strict=True)
     else:
@@ -90,21 +93,25 @@ def run():
     net.to(device)
     if torch.cuda.device_count() > 1:
         from utils.helper_funcs import parse_gpu_ids
-        args['gpu_ids'] = [i for i in range(torch.cuda.device_count())]
-        net = torch.nn.DataParallel(net, device_ids=args['gpu_ids'])
-        net.to('cuda:0')
+
+        args["gpu_ids"] = [i for i in range(torch.cuda.device_count())]
+        net = torch.nn.DataParallel(net, device_ids=args["gpu_ids"])
+        net.to("cuda:0")
     net.eval()
     #######################
     # Create data loaders #
     #######################
-    if args['dataset'] == 'esc50':
+    if args["dataset"] == "esc50":
         from datasets.esc_dataset import ESCDataset as SoundDataset
-        data_set = SoundDataset(args['data_path'],
-                                mode='test',
-                                segment_length=args['seq_len'],
-                                sampling_rate=args['sampling_rate'],
-                                fold_id=args['fold_id'],
-                                transforms=None)
+
+        data_set = SoundDataset(
+            args["data_path"],
+            mode="test",
+            segment_length=args["seq_len"],
+            sampling_rate=args["sampling_rate"],
+            fold_id=args["fold_id"],
+            transforms=None,
+        )
 
     elif args["dataset"] == "kpf":
         from datasets.kpf_dataset import KpfDatasetPath as SoundDataset
@@ -120,43 +127,51 @@ def run():
         )
         data_set.filter({"golden_set": {args["task"]: True}})
 
-    elif args['dataset'] == 'speechcommands':
+    elif args["dataset"] == "speechcommands":
         from datasets.speechcommand_dataset import SpeechCommandsDataset as SoundDataset
-        data_set = SoundDataset(args['data_path'],
-                                mode='test',
-                                segment_length=args['seq_len'],
-                                sampling_rate=args['sampling_rate'],
-                                transforms=None)
 
-    elif args['dataset'] == 'urban8k':
-        from datasets.urban8K_dataset import Urban8KDataset as SoundDataset
-        data_set = SoundDataset(args['data_path'],
-                                mode='test',
-                                segment_length=args['seq_len'],
-                                sampling_rate=args['sampling_rate'],
-                                transforms=None,
-                                fold_id=args['fold_id'])
-
-    elif args['dataset'] == 'audioset':
-        from datasets.audioset_dataset import AudioSetDataset as SoundDataset
         data_set = SoundDataset(
-            args['data_path'],
-            'test',
+            args["data_path"],
+            mode="test",
+            segment_length=args["seq_len"],
+            sampling_rate=args["sampling_rate"],
+            transforms=None,
+        )
+
+    elif args["dataset"] == "urban8k":
+        from datasets.urban8K_dataset import Urban8KDataset as SoundDataset
+
+        data_set = SoundDataset(
+            args["data_path"],
+            mode="test",
+            segment_length=args["seq_len"],
+            sampling_rate=args["sampling_rate"],
+            transforms=None,
+            fold_id=args["fold_id"],
+        )
+
+    elif args["dataset"] == "audioset":
+        from datasets.audioset_dataset import AudioSetDataset as SoundDataset
+
+        data_set = SoundDataset(
+            args["data_path"],
+            "test",
             data_subtype=None,
-            segment_length=args['seq_len'],
-            sampling_rate=args['sampling_rate'],
-            transforms=None
+            segment_length=args["seq_len"],
+            sampling_rate=args["sampling_rate"],
+            transforms=None,
         )
 
     else:
         raise ValueError
 
-    if args['dataset'] != 'audioset':
+    if args["dataset"] != "audioset":
         inference_single_label(net=net, data_set=data_set, args=args)
-    elif args['dataset'] == 'audioset':
+    elif args["dataset"] == "audioset":
         inference_multi_label(net=net, data_set=data_set, args=args)
     else:
         raise ValueError("check args dataset")
+
 
 def inference_single_label(net, data_set, args):
     from utils.helper_funcs import collate_fn_keep_dict
@@ -177,9 +192,9 @@ def inference_single_label(net, data_set, args):
     converter = LabelConverter(task=args["task"])
 
     labels = torch.zeros(len(data_loader.dataset), dtype=torch.float32).float()
-    preds = torch.zeros(len(data_loader.dataset), args['n_classes'], dtype=torch.float32).float()
+    preds = torch.zeros(len(data_loader.dataset), args["n_classes"], dtype=torch.float32).float()
     # confusion_matrix = torch.zeros(args['n_classes'], args['n_classes'], dtype=torch.int)
-    confusion_matrix = torch.zeros(args['n_classes'], args['n_classes'], dtype=torch.int)
+    confusion_matrix = torch.zeros(args["n_classes"], args["n_classes"], dtype=torch.int)
     idx_start = 0
     results = []
     for i, (x, y) in enumerate(data_loader):
@@ -202,12 +217,32 @@ def inference_single_label(net, data_set, args):
                         "label": converter.convert_model_output_to_label(_y),
                         "pred": converter.convert_model_output_to_label(_pred),
                         "time_interval": _time_interval,
-                    } for _path, _y, _pred, _time_interval in zip(y["path"], y_target.tolist(), y_est.tolist(), y["time_interval"])
+                        "fmso": _fmso,
+                        "key": _key,
+                        "level": _level,
+                        "properties": _properties,
+                    }
+                    for _path, _y, _pred, _time_interval, _fmso, _key, _level, _properties in zip(
+                        y["path"],
+                        y_target.tolist(),
+                        y_est.tolist(),
+                        y["time_interval"],
+                        y["fmso"],
+                        y["key"],
+                        y["level"],
+                        y["properties"],
+                    )
                 ]
             )
         idx_start = idx_end
-    pd.DataFrame(results).to_csv(args['f_res'] / f"results_{args['task']}.csv", index=False)
-    acc_av = accuracy(preds.detach(), labels.detach(), [1, ])[0]
+    pd.DataFrame(results).to_csv(args["f_res"] / f"results_{args['task']}.csv", index=False)
+    acc_av = accuracy(
+        preds.detach(),
+        labels.detach(),
+        [
+            1,
+        ],
+    )[0]
 
     res = {
         "acc": acc_av.item(),
@@ -215,12 +250,12 @@ def inference_single_label(net, data_set, args):
         "labels": labels.view(-1).tolist(),
         "confusion_matrix": confusion_matrix.tolist(),
     }
-    save_json(args['f_res'] / "res.json", res)
+    save_json(args["f_res"] / "res.json", res)
     print(f"result saved at:{args['f_res'] / 'res.json'}")
 
-    print("acc:{}".format(np.round(acc_av*100)/100))
+    print("acc:{}".format(np.round(acc_av * 100) / 100))
     print("cm:{}".format(confusion_matrix.diag().sum() / len(data_loader.dataset)))
-    print('***************************************')
+    print("***************************************")
     # bad_labels = []
     # for i, c in enumerate(confusion_matrix):
     #     i_est = c.argmax(-1)
@@ -233,22 +268,25 @@ def inference_single_label(net, data_set, args):
 
 def inference_multi_label(net, data_set, args):
     from utils.helper_funcs import collate_fn
-    data_loader = DataLoader(data_set,
-                             batch_size=128,
-                             num_workers=8,
-                             pin_memory=True if torch.cuda.is_available() else False,
-                             shuffle=False,
-                             collate_fn=collate_fn)
 
-    labels = torch.zeros(len(data_loader.dataset), args['n_classes'], dtype=torch.float32).float()
-    preds = torch.zeros(len(data_loader.dataset), args['n_classes'], dtype=torch.float32).float()
-    confusion_matrix = torch.zeros(args['n_classes'], args['n_classes'], dtype=torch.int)
+    data_loader = DataLoader(
+        data_set,
+        batch_size=128,
+        num_workers=8,
+        pin_memory=True if torch.cuda.is_available() else False,
+        shuffle=False,
+        collate_fn=collate_fn,
+    )
+
+    labels = torch.zeros(len(data_loader.dataset), args["n_classes"], dtype=torch.float32).float()
+    preds = torch.zeros(len(data_loader.dataset), args["n_classes"], dtype=torch.float32).float()
+    confusion_matrix = torch.zeros(args["n_classes"], args["n_classes"], dtype=torch.int)
     idx_start = 0
     for i, (x, y) in enumerate(data_loader):
         with torch.no_grad():
-            x = x.to('cuda:0')
-            y = [F.one_hot(torch.Tensor(y_i).long(), args['n_classes']).sum(dim=0).float() for y_i in y]
-            y = torch.stack(y, dim=0).contiguous().to('cuda:0')
+            x = x.to("cuda:0")
+            y = [F.one_hot(torch.Tensor(y_i).long(), args["n_classes"]).sum(dim=0).float() for y_i in y]
+            y = torch.stack(y, dim=0).contiguous().to("cuda:0")
             pred = net(x)
             idx_end = idx_start + y.shape[0]
             preds[idx_start:idx_end, :] = torch.sigmoid(pred)
@@ -260,12 +298,12 @@ def inference_multi_label(net, data_set, args):
         "mAP": mAP_av,
         "preds": preds,
         "labels": labels.view(-1),
-        "confusion_matrix": confusion_matrix
+        "confusion_matrix": confusion_matrix,
     }
-    torch.save(res, args['f_res'] / "res.pt")
+    torch.save(res, args["f_res"] / "res.pt")
     # torch.save(net.state_dict(), "net.pt")
-    print("mAP:{}".format(np.round(mAP_av*100)/100))
+    print("mAP:{}".format(np.round(mAP_av * 100) / 100))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run()
